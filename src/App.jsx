81a82eb961e6593d3f8b08f5fc44530cc1c37701
Samet Aslan <sametaslan1710@gmail.com>
2023-12-29 11:28:59 +0300

    add: Basic Structure of Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-
 import './App.css'
-
+import Favorites from './components/Favorites'
+import Meals from './components/Meals'
+import Modal from './components/Modal'
+import Search from './components/Search'
 function App() {
-  
+
 
   return (
-    <></>
+    <main>
+      <Meals />
+      {/* <Favorites /> */}
+      {/* <Search />
+      <Modal /> */}
+    </main>
   )
 }
 
---
+++ src/components/Favorites.jsx
@@ -0,0 +1,8 @@
+
+
+const Favorites = () =>{
+    return(
+        <h1>Favorites Component</h1>
+    )
+}
+export default Favorites;
