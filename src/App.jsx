5f0348c5fb072bdd1793940fdc5c6bc2c59d14b2
Samet Aslan <sametaslan1710@gmail.com>
2023-12-30 20:56:54 +0300

    add: Meals component , General CSS styles. fix: data fetching with axios

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Meals />
       {/* <Favorites /> */}
       {/* <Search />
-      <Modal /> */}
+        <Modal /> */}
     </main>
   )
 }
--- src/context.jsx
+++ src/context.jsx
@@ -1,16 +1,50 @@
-import React, { useContext } from "react";
-
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
 const AppContext = React.createContext()
 
+
+const allMealsURL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a';
+const randomMealURL = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+
 const AppProvider = ({ children }) => {
+    const [loading, setLoading] = useState(false);
+    const [meals, setMeals] = useState([]);
+
+    const fetchMeals = async (url) => {
+        setLoading(true);
+        try {
+            const { data } = await axios(url);
+            if(data.meals){
+                setMeals(data.meals)
+            }
+            else{
+                setMeals([])
+            }
+            
+
+        } catch (error) {
+            console.log(error.response)
+        }
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchMeals(allMealsURL);
+    }, []);
+
+
+
+
+
     return (
-        <AppContext.Provider value={{name:'john', role:"student"}} >
+        <AppContext.Provider value={{ loading, meals }} >
             {children}
         </AppContext.Provider>
     )
 }
 
-export const useGlobalContext = () =>{
+export const useGlobalContext = () => {
     return useContext(AppContext)
 }
 
